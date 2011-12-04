bd570fb9a8f54e85b3699b76370bfddb2249c437
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-12-04 19:24:15 +0100

    Add a test to endpoint

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
+#include "interfaces/endpoint.h"
+#include <glib.h>
 
 int main(int argc, char **argv) {
+	GObject *ep;
+	GValue val = G_VALUE_INIT;
 
-    return 0;
+	g_type_init();
+
+	ep = g_object_new(KMS_TYPE_ENDPOINT, "localname", "test_ep", NULL);
+
+	if (ep == NULL) {
+		g_print("Create endpont is: NULL\n");
+		return 1;
+	}
+
+	g_value_init(&val, G_TYPE_STRING);
+
+	g_object_get_property(ep, "localname", &val);
+
+	g_print("Created ep with localname: %s\n", g_value_get_string(&val));
+
+	return 0;
 }
