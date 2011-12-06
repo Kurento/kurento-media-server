e85c826a51fdd820346443cfa62b0d95b4072811
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-12-06 10:01:12 +0100

    Codding style in kms-media-handler-manager

--- src/kms-media-handler-manager.h
+++ src/kms-media-handler-manager.h
@@ -3,13 +3,13 @@
 
 #include <glib-object.h>
 
-#define KMS_TYPE_MEDIA_HANDLER_MANAGER                 (kms_media_handler_manager_get_type())
-#define KMS_MEDIA_HANDLER_MANAGER(obj)                 (G_TYPE_CHECK_INSTANCE_CAST((obj), KMS_TYPE_MEDIA_HANDLER_MANAGER, KmsMediaHandlerManager))
-#define KMS_IS_MEDIA_HANDLER_MANAGER(obj)              (G_TYPE_CHECK_INSTANCE_TYPE((obj), KMS_TYPE_MEDIA_HANDLER_MANAGER))
-#define KMS_MEDIA_HANDLER_MANAGER_GET_INTERFACE(inst)  (G_TYPE_INSTANCE_GET_INTERFACE((inst), KMS_TYPE_MEDIA_HANDLER_MANAGER, KmsMediaHandlerManagerInterface))
+#define KMS_TYPE_MEDIA_HANDLER_MANAGER			(kms_media_handler_manager_get_type())
+#define KMS_MEDIA_HANDLER_MANAGER(obj)			(G_TYPE_CHECK_INSTANCE_CAST((obj), KMS_TYPE_MEDIA_HANDLER_MANAGER, KmsMediaHandlerManager))
+#define KMS_IS_MEDIA_HANDLER_MANAGER(obj)		(G_TYPE_CHECK_INSTANCE_TYPE((obj), KMS_TYPE_MEDIA_HANDLER_MANAGER))
+#define KMS_MEDIA_HANDLER_MANAGER_GET_INTERFACE(inst)	(G_TYPE_INSTANCE_GET_INTERFACE((inst), KMS_TYPE_MEDIA_HANDLER_MANAGER, KmsMediaHandlerManagerInterface))
 
-typedef struct _KmsMediaHandlerManager               KmsMediaHandlerManager; /* dummy object */
-typedef struct _KmsMediaHandlerManagerInterface      KmsMediaHandlerManagerInterface;
+typedef struct _KmsMediaHandlerManager		KmsMediaHandlerManager; /* dummy object */
+typedef struct _KmsMediaHandlerManagerInterface	KmsMediaHandlerManagerInterface;
 
 struct _KmsMediaHandlerManagerInterface
 {
@@ -19,7 +19,7 @@ struct _KmsMediaHandlerManagerInterface
 	void (*get_factory)(KmsMediaHandlerManager *self);
 };
 
-GType kms_media_handler_manager_get_type (void);
+GType kms_media_handler_manager_get_type(void);
 
 /* TODO: make this function return a KmsMediaHandlerFactory */
 void kms_media_handler_manager_get_factory(KmsMediaHandlerManager *self);
