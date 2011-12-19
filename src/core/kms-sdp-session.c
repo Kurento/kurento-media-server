b919e1f7af95294298b36d32de66f28054ed50dd
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-12-19 21:02:49 +0100

    Add more properties to sdp-session

--- src/core/kms-sdp-session.c
+++ src/core/kms-sdp-session.c
@@ -10,13 +10,11 @@ struct _KmsSdpSessionPriv {
 	gchar *addr;
 	GValueArray *medias;
 	gchar *name;
-	/*
-	sdpVersion
-	username
-	sessionId
-	version
-	remoteHandler
-	 */
+	glong id;
+	glong version;
+	gint sdp_version;
+	gchar *remote_handler;
+	gchar *username;
 };
 
 enum {
@@ -24,7 +22,12 @@ enum {
 
 	PROP_ADDR,
 	PROP_MEDIAS,
-	PROP_NAME
+	PROP_NAME,
+	PROP_ID,
+	PROP_VERSION,
+	PROP_SDP_VERSION,
+	PROP_REMOTE_HANDLER,
+	PROP_USERNAME
 };
 
 G_DEFINE_TYPE(KmsSdpSession, kms_sdp_session, G_TYPE_OBJECT)
@@ -42,6 +45,22 @@ free_name(KmsSdpSession *self) {
 	if (self->priv->name != NULL) {
 		g_free(self->priv->name);
 		self->priv->name = NULL;
+	}
+}
+
+static void
+free_remote_handler(KmsSdpSession *self) {
+	if (self->priv->remote_handler != NULL) {
+		g_free(self->priv->remote_handler);
+		self->priv->remote_handler = NULL;
+	}
+}
+
+static void
+free_username(KmsSdpSession *self) {
+	if (self->priv->username != NULL) {
+		g_free(self->priv->username);
+		self->priv->username = NULL;
 	}
 }
 
@@ -95,6 +114,35 @@ kms_sdp_session_set_property(GObject  *object, guint property_id,
 			self->priv->name = g_value_dup_string(value);
 			UNLOCK(self);
 			break;
+		case PROP_SDP_VERSION:
+			LOCK(self);
+			self->priv->sdp_version = g_value_get_int(value);
+			UNLOCK(self);
+			break;
+		case PROP_REMOTE_HANDLER:
+			LOCK(self);
+			free_remote_handler(self);
+			self->priv->remote_handler = g_value_dup_string(value);
+			UNLOCK(self);
+			break;
+		case PROP_USERNAME:
+			LOCK(self);
+			free_username(self);
+			self->priv->username = g_value_dup_string(value);
+			UNLOCK(self);
+			break;
+		case PROP_ID:
+			LOCK(self);
+			free_username(self);
+			self->priv->id = g_value_get_long(value);
+			UNLOCK(self);
+			break;
+		case PROP_VERSION:
+			LOCK(self);
+			free_username(self);
+			self->priv->version = g_value_get_long(value);
+			UNLOCK(self);
+			break;
 		default:
 			/* We don't have any other property... */
 			G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
@@ -125,6 +173,31 @@ kms_sdp_session_get_property(GObject *object, guint property_id, GValue *value,
 			g_value_set_string(value, self->priv->name);
 			UNLOCK(self);
 			break;
+		case PROP_SDP_VERSION:
+			LOCK(self);
+			g_value_set_int(value, self->priv->sdp_version);
+			UNLOCK(self);
+			break;
+		case PROP_REMOTE_HANDLER:
+			LOCK(self);
+			g_value_set_string(value, self->priv->remote_handler);
+			UNLOCK(self);
+			break;
+		case PROP_USERNAME:
+			LOCK(self);
+			g_value_set_string(value, self->priv->username);
+			UNLOCK(self);
+			break;
+		case PROP_ID:
+			LOCK(self);
+			g_value_set_long(value, self->priv->id);
+			UNLOCK(self);
+			break;
+		case PROP_VERSION:
+			LOCK(self);
+			g_value_set_long(value, self->priv->version);
+			UNLOCK(self);
+			break;
 		default:
 			/* We don't have any other property... */
 			G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
@@ -147,6 +220,8 @@ kms_sdp_session_finalize(GObject *object) {
 
 	free_addr(self);
 	free_name(self);
+	free_remote_handler(self);
+	free_username(self);
 	g_static_mutex_free(&(self->priv->mutex));
 	/* Chain up to the parent class */
 	G_OBJECT_CLASS (kms_sdp_session_parent_class)->finalize(object);
@@ -166,7 +241,7 @@ kms_sdp_session_class_init(KmsSdpSessionClass *klass) {
 
 	pspec = g_param_spec_string("address", "Address",
 					"Remote address",
-					"",
+					"-",
 					G_PARAM_CONSTRUCT_ONLY |
 					G_PARAM_READWRITE);
 
@@ -189,6 +264,46 @@ kms_sdp_session_class_init(KmsSdpSessionClass *klass) {
 					G_PARAM_READWRITE);
 
 	g_object_class_install_property(gobject_class, PROP_NAME, pspec);
+
+	pspec = g_param_spec_int("sdp_version", "SDP Version",
+					"SDP protocol version",
+					0, 2, 0,
+					G_PARAM_CONSTRUCT_ONLY |
+					G_PARAM_READWRITE);
+
+	g_object_class_install_property(gobject_class, PROP_SDP_VERSION, pspec);
+
+	pspec = g_param_spec_string("remote_handler", "Remote handler",
+					"Remote handler",
+					"",
+					G_PARAM_CONSTRUCT_ONLY |
+					G_PARAM_READWRITE);
+
+	g_object_class_install_property(gobject_class, PROP_REMOTE_HANDLER, pspec);
+
+	pspec = g_param_spec_string("username", "Username",
+					"User name",
+					"-",
+					G_PARAM_CONSTRUCT_ONLY |
+					G_PARAM_READWRITE);
+
+	g_object_class_install_property(gobject_class, PROP_USERNAME, pspec);
+
+	pspec = g_param_spec_long("id", "Session identifier",
+					"Session identifier",
+					0L, G_MAXLONG, 0L,
+					G_PARAM_CONSTRUCT_ONLY |
+					G_PARAM_READWRITE);
+
+	g_object_class_install_property(gobject_class, PROP_ID, pspec);
+
+	pspec = g_param_spec_long("version", "Session Version",
+					"Session Version",
+					0L, G_MAXLONG, 0L,
+					G_PARAM_CONSTRUCT_ONLY |
+					G_PARAM_READWRITE);
+
+	g_object_class_install_property(gobject_class, PROP_VERSION, pspec);
 }
 
 static void
@@ -199,4 +314,9 @@ kms_sdp_session_init(KmsSdpSession *self) {
 	self->priv->addr = NULL;
 	self->priv->medias = NULL;
 	self->priv->name = NULL;
+	self->priv->id = 0L;
+	self->priv->version = 0L;
+	self->priv->sdp_version = 0;
+	self->priv->remote_handler = NULL;
+	self->priv->username = NULL;
 }
