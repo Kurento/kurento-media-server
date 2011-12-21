a3f6fcca8917f432321e6a7e66d0db9d0430a0b1
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-12-21 12:36:39 +0100

    Make media-handler-sink to be an object

--- src/core/kms-media-handler-sink.c
+++ src/core/kms-media-handler-sink.c
@@ -1,30 +1,27 @@
 #include <kms-core.h>
 
+#define KMS_MEDIA_HANDLER_SINK_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), KMS_TYPE_MEDIA_HANDLER_SINK, KmsMediaHandlerSinkPriv))
+
+#define LOCK(obj) (g_static_mutex_lock(&(KMS_MEDIA_HANDLER_SINK(obj)->priv->mutex)))
+#define UNLOCK(obj) (g_static_mutex_unlock(&(KMS_MEDIA_HANDLER_SINK(obj)->priv->mutex)))
+
+struct _KmsMediaHandlerSinkPriv {
+	GStaticMutex mutex;
+};
+
+enum {
+	PROP_0,
+
+};
+
+G_DEFINE_TYPE(KmsMediaHandlerSink, kms_media_handler_sink, G_TYPE_OBJECT)
+
 static void
-kms_media_handler_sink_base_init (gpointer g_class) {
-	static gboolean is_initialized = FALSE;
-
-	if (!is_initialized) {
-		/* add properties and signals to the interface here */
-
-		is_initialized = TRUE;
-	}
+kms_media_handler_sink_class_init(KmsMediaHandlerSinkClass *klass) {
+	g_type_class_add_private(klass, sizeof(KmsMediaHandlerSinkPriv));
 }
 
-GType
-kms_media_handler_sink_get_type(void) {
-	static GType iface_type = 0;
-	if (iface_type == 0) {
-		static const GTypeInfo info = {
-			sizeof (KmsMediaHandlerSinkInterface),
-			kms_media_handler_sink_base_init,   /* base_init */
-			NULL,   /* base_finalize */
-		};
-
-		iface_type = g_type_register_static(G_TYPE_INTERFACE,
-						"KmsMediaHandlerSink",
-						&info, 0);
-	}
-
-	return iface_type;
+static void
+kms_media_handler_sink_init(KmsMediaHandlerSink *self) {
+	self->priv = KMS_MEDIA_HANDLER_SINK_GET_PRIVATE(self);
 }
--- src/core/kms-media-handler-sink.h
+++ src/core/kms-media-handler-sink.h
@@ -7,19 +7,29 @@
 
 #include <glib-object.h>
 
-#define KMS_TYPE_MEDIA_HANDLER_SINK			(kms_media_handler_sink_get_type())
-#define KMS_MEDIA_HANDLER_SINK(obj)			(G_TYPE_CHECK_INSTANCE_CAST((obj), KMS_TYPE_MEDIA_HANDLER_SINK, KmsMediaHandlerSink))
-#define KMS_IS_MEDIA_HANDLER_SINK(obj)			(G_TYPE_CHECK_INSTANCE_TYPE((obj), KMS_TYPE_MEDIA_HANDLER_SINK))
-#define KMS_MEDIA_HANDLER_SINK_GET_INTERFACE(inst)	(G_TYPE_INSTANCE_GET_INTERFACE((inst), KMS_TYPE_MEDIA_HANDLER_SINK, KmsMediaHandlerSinkInterface))
+#define KMS_TYPE_MEDIA_HANDLER_SINK		(kms_media_handler_sink_get_type())
+#define KMS_MEDIA_HANDLER_SINK(obj)		(G_TYPE_CHECK_INSTANCE_CAST((obj), KMS_TYPE_MEDIA_HANDLER_SINK, KmsMediaHandlerSink))
+#define KMS_IS_MEDIA_HANDLER_SINK(obj)		(G_TYPE_CHECK_INSTANCE_TYPE((obj), KMS_TYPE_MEDIA_HANDLER_SINK))
+#define KMS_MEDIA_HANDLER_SINK_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), KMS_TYPE_MEDIA_HANGER_SINK, KmsMediaHandlerSinkClass))
+#define KMS_IS_MEDIA_HANDLER_SINK_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), KMS_TYPE_MEDIA_HANDLER_SINK))
+#define KMS_MEDIA_HANDLER_SINK_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), KMS_TYPE_MEDIA_HANDLER_SINK, KmsMediaHandlerSinkClass))
 
-typedef struct _KmsMediaHandlerSink		KmsMediaHandlerSink; /* dummy object */
-typedef struct _KmsMediaHandlerSinkInterface	KmsMediaHandlerSinkInterface;
+typedef struct _KmsMediaHandlerSink		KmsMediaHandlerSink;
+typedef struct _KmsMediaHandlerSinkClass	KmsMediaHandlerSinkClass;
+typedef struct _KmsMediaHandlerSinkPriv		KmsMediaHandlerSinkPriv;
 
-struct _KmsMediaHandlerSinkInterface {
-	GTypeInterface parent_iface;
+struct _KmsMediaHandlerSink {
+	GObject parent_instance;
 
-	/* Virtual methods */
-	/* TODO: Add methods */
+	/* instance members */
+
+	KmsMediaHandlerSinkPriv *priv;
+};
+
+struct _KmsMediaHandlerSinkClass {
+	GObjectClass parent_class;
+
+	/* class members */
 };
 
 GType kms_media_handler_sink_get_type (void);
