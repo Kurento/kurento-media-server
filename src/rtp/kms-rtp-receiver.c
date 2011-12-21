27fe80338dfd2cb58b281da333774e7f3b32c0c4
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-12-21 14:24:35 +0100

    Prepare rtp-receiver to handle local-spec property

--- src/rtp/kms-rtp-receiver.c
+++ src/rtp/kms-rtp-receiver.c
@@ -8,6 +8,7 @@
 
 struct _KmsRtpReceiverPriv {
 	GStaticMutex mutex;
+	KmsSdpMedia *local_spec;
 };
 
 enum {
@@ -18,11 +19,49 @@ enum {
 G_DEFINE_TYPE(KmsRtpReceiver, kms_rtp_receiver, G_TYPE_OBJECT)
 
 static void
+dispose_local_spec(KmsRtpReceiver *self) {
+	if (self->priv->local_spec != NULL) {
+		g_object_unref(self->priv->local_spec);
+		self->priv->local_spec = NULL;
+	}
+}
+
+static void
+dispose(GObject *object) {
+	KmsRtpReceiver *self = KMS_RTP_RECEIVER(object);
+
+	LOCK(self);
+	dispose_local_spec(self);
+	UNLOCK(self);
+
+	/* Chain up to the parent class */
+	G_OBJECT_CLASS(kms_rtp_receiver_parent_class)->dispose(object);
+}
+
+static void
+finalize(GObject *object) {
+	KmsRtpReceiver *self = KMS_RTP_RECEIVER(object);
+
+	g_static_mutex_free(&(self->priv->mutex));
+
+	/* Chain up to the parent class */
+	G_OBJECT_CLASS(kms_rtp_receiver_parent_class)->finalize(object);
+}
+
+static void
 kms_rtp_receiver_class_init(KmsRtpReceiverClass *klass) {
+	GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
 	g_type_class_add_private(klass, sizeof(KmsRtpReceiverPriv));
+
+	object_class->finalize = finalize;
+	object_class->dispose = dispose;
 }
 
 static void
 kms_rtp_receiver_init(KmsRtpReceiver *self) {
 	self->priv = KMS_RTP_RECEIVER_GET_PRIVATE(self);
+
+	g_static_mutex_init(&(self->priv->mutex));
+	self->priv->local_spec = NULL;
 }
