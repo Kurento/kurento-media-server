43e61cd154fbb485473c27c189539549cb669f39
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-12-21 12:41:46 +0100

    Make media-handler-src to be an object

--- src/core/kms-media-handler-src.c
+++ src/core/kms-media-handler-src.c
@@ -1,30 +1,27 @@
 #include <kms-core.h>
 
+#define KMS_MEDIA_HANDLER_SRC_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), KMS_TYPE_MEDIA_HANDLER_SRC, KmsMediaHandlerSrcPriv))
+
+#define LOCK(obj) (g_static_mutex_lock(&(KMS_MEDIA_HANDLER_SRC(obj)->priv->mutex)))
+#define UNLOCK(obj) (g_static_mutex_unlock(&(KMS_MEDIA_HANDLER_SRC(obj)->priv->mutex)))
+
+struct _KmsMediaHandlerSrcPriv {
+	GStaticMutex mutex;
+};
+
+enum {
+	PROP_0,
+
+};
+
+G_DEFINE_TYPE(KmsMediaHandlerSrc, kms_media_handler_src, G_TYPE_OBJECT)
+
 static void
-kms_media_handler_src_base_init (gpointer g_class) {
-	static gboolean is_initialized = FALSE;
-
-	if (!is_initialized) {
-		/* add properties and signals to the interface here */
-
-		is_initialized = TRUE;
-	}
+kms_media_handler_src_class_init(KmsMediaHandlerSrcClass *klass) {
+	g_type_class_add_private(klass, sizeof(KmsMediaHandlerSrcPriv));
 }
 
-GType
-kms_media_handler_src_get_type(void) {
-	static GType iface_type = 0;
-	if (iface_type == 0) {
-		static const GTypeInfo info = {
-			sizeof (KmsMediaHandlerSrcInterface),
-			kms_media_handler_src_base_init,   /* base_init */
-			NULL,   /* base_finalize */
-		};
-
-		iface_type = g_type_register_static(G_TYPE_INTERFACE,
-						"KmsMediaHandlerSrc",
-						&info, 0);
-	}
-
-	return iface_type;
+static void
+kms_media_handler_src_init(KmsMediaHandlerSrc *self) {
+	self->priv = KMS_MEDIA_HANDLER_SRC_GET_PRIVATE(self);
 }
--- src/core/kms-media-handler-src.h
+++ src/core/kms-media-handler-src.h
@@ -7,19 +7,29 @@
 
 #include <glib-object.h>
 
-#define KMS_TYPE_MEDIA_HANDLER_SRC			(kms_media_handler_src_get_type())
-#define KMS_MEDIA_HANDLER_SRC(obj)			(G_TYPE_CHECK_INSTANCE_CAST((obj), KMS_TYPE_MEDIA_HANDLER_SRC, KmsMediaHandlerSrc))
+#define KMS_TYPE_MEDIA_HANDLER_SRC		(kms_media_handler_src_get_type())
+#define KMS_MEDIA_HANDLER_SRC(obj)		(G_TYPE_CHECK_INSTANCE_CAST((obj), KMS_TYPE_MEDIA_HANDLER_SRC, KmsMediaHandlerSrc))
 #define KMS_IS_MEDIA_HANDLER_SRC(obj)		(G_TYPE_CHECK_INSTANCE_TYPE((obj), KMS_TYPE_MEDIA_HANDLER_SRC))
-#define KMS_MEDIA_HANDLER_SRC_GET_INTERFACE(inst)	(G_TYPE_INSTANCE_GET_INTERFACE((inst), KMS_TYPE_MEDIA_HANDLER_SRC, KmsMediaHandlerSrcInterface))
+#define KMS_MEDIA_HANDLER_SRC_CLASS(klass)	(G_TYPE_CHECK_CLASS_CAST ((klass), KMS_TYPE_MEDIA_HANGER_SRC, KmsMediaHandlerSrcClass))
+#define KMS_IS_MEDIA_HANDLER_SRC_CLASS(klass)	(G_TYPE_CHECK_CLASS_TYPE ((klass), KMS_TYPE_MEDIA_HANDLER_SRC))
+#define KMS_MEDIA_HANDLER_SRC_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS ((obj), KMS_TYPE_MEDIA_HANDLER_SRC, KmsMediaHandlerSrcClass))
 
-typedef struct _KmsMediaHandlerSrc		KmsMediaHandlerSrc; /* dummy object */
-typedef struct _KmsMediaHandlerSrcInterface	KmsMediaHandlerSrcInterface;
+typedef struct _KmsMediaHandlerSrc		KmsMediaHandlerSrc;
+typedef struct _KmsMediaHandlerSrcClass		KmsMediaHandlerSrcClass;
+typedef struct _KmsMediaHandlerSrcPriv		KmsMediaHandlerSrcPriv;
 
-struct _KmsMediaHandlerSrcInterface {
-	GTypeInterface parent_iface;
+struct _KmsMediaHandlerSrc {
+	GObject parent_instance;
 
-	/* Virtual methods */
-	/* TODO: Add methods */
+	/* instance members */
+
+	KmsMediaHandlerSrcPriv *priv;
+};
+
+struct _KmsMediaHandlerSrcClass {
+	GObjectClass parent_class;
+
+	/* class members */
 };
 
 GType kms_media_handler_src_get_type (void);
