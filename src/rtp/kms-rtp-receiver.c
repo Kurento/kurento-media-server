cd7b3c36deb2881667d24bc8d1b9ec44631a157f
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-12-21 14:36:02 +0100

    Handle local-spec property in rtp-receiver

--- src/rtp/kms-rtp-receiver.c
+++ src/rtp/kms-rtp-receiver.c
@@ -14,6 +14,7 @@ struct _KmsRtpReceiverPriv {
 enum {
 	PROP_0,
 
+	PROP_LOCAL_SPEC,
 };
 
 G_DEFINE_TYPE(KmsRtpReceiver, kms_rtp_receiver, G_TYPE_OBJECT)
@@ -23,6 +24,43 @@ dispose_local_spec(KmsRtpReceiver *self) {
 	if (self->priv->local_spec != NULL) {
 		g_object_unref(self->priv->local_spec);
 		self->priv->local_spec = NULL;
+	}
+}
+
+static void
+set_property (GObject *object, guint property_id, const GValue *value,
+							GParamSpec *pspec) {
+	KmsRtpReceiver *self = KMS_RTP_RECEIVER(object);
+
+	switch (property_id) {
+		case PROP_LOCAL_SPEC:
+			LOCK(self);
+			dispose_local_spec(self);
+			self->priv->local_spec = g_value_dup_object(value);
+			UNLOCK(self);
+			break;
+		default:
+			/* We don't have any other property... */
+			G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
+			break;
+	}
+}
+
+static void
+get_property(GObject *object, guint property_id, GValue *value,
+							GParamSpec *pspec) {
+	KmsRtpReceiver *self = KMS_RTP_RECEIVER(object);
+
+	switch (property_id) {
+		case PROP_LOCAL_SPEC:
+			LOCK(self);
+			g_value_set_object(value, self->priv->local_spec);
+			UNLOCK(self);
+			break;
+		default:
+			/* We don't have any other property... */
+			G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
+			break;
 	}
 }
 
@@ -50,12 +88,23 @@ finalize(GObject *object) {
 
 static void
 kms_rtp_receiver_class_init(KmsRtpReceiverClass *klass) {
+	GParamSpec *pspec;
 	GObjectClass *object_class = G_OBJECT_CLASS(klass);
 
 	g_type_class_add_private(klass, sizeof(KmsRtpReceiverPriv));
 
 	object_class->finalize = finalize;
 	object_class->dispose = dispose;
+	object_class->set_property = set_property;
+	object_class->get_property = get_property;
+
+	pspec = g_param_spec_object("local-spec", "Local Session Spec",
+					"Local Session Spec",
+					KMS_TYPE_SDP_SESSION,
+					G_PARAM_CONSTRUCT_ONLY |
+					G_PARAM_READWRITE);
+
+	g_object_class_install_property(object_class, PROP_LOCAL_SPEC, pspec);
 }
 
 static void
