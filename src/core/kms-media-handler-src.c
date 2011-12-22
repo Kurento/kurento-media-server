5464107075c34cd567c4168f9c190d26d431aea8
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2011-12-22 16:11:19 +0100

    Move Bin to parent class
    Now media-handler-src inherits from bin and no extre bin is needed

--- src/core/kms-media-handler-src.c
+++ src/core/kms-media-handler-src.c
@@ -5,6 +5,8 @@
 #define LOCK(obj) (g_static_mutex_lock(&(KMS_MEDIA_HANDLER_SRC(obj)->priv->mutex)))
 #define UNLOCK(obj) (g_static_mutex_unlock(&(KMS_MEDIA_HANDLER_SRC(obj)->priv->mutex)))
 
+G_LOCK_DEFINE_STATIC(class_lock);
+
 struct _KmsMediaHandlerSrcPriv {
 	GStaticMutex mutex;
 };
@@ -14,14 +16,74 @@ enum {
 
 };
 
-G_DEFINE_TYPE(KmsMediaHandlerSrc, kms_media_handler_src, G_TYPE_OBJECT)
+G_DEFINE_TYPE(KmsMediaHandlerSrc, kms_media_handler_src, GST_TYPE_BIN)
+
+static gchar*
+get_name(KmsMediaHandlerSrc *self) {
+	static glong count = 0;
+	gchar *name;
+
+	G_LOCK(class_lock);
+	name = g_strdup_printf("%s-%ld", G_OBJECT_TYPE_NAME(self), count++);
+	G_UNLOCK(class_lock);
+
+	return name;
+}
+
+static void
+constructed(GObject *object) {
+	KmsMediaHandlerSrc *self = KMS_MEDIA_HANDLER_SRC(object);
+	GstElement *pipe, *bin;
+	gchar *name;
+
+	name = get_name(self);
+	gst_element_set_name(self, name);
+	g_free(name);
+
+	bin = GST_ELEMENT(self);
+
+	g_object_set(bin, "async-handling", TRUE, NULL);
+	GST_OBJECT_FLAG_SET(bin, GST_ELEMENT_LOCKED_STATE);
+	gst_element_set_state(bin, GST_STATE_PLAYING);
+
+	pipe = kms_get_pipeline();
+	gst_bin_add(GST_BIN(pipe), bin);
+}
+
+static void
+finalize(GObject *object) {
+	/**/
+
+	G_OBJECT_CLASS(kms_media_handler_src_parent_class)->finalize(object);
+}
+
+static void
+dispose(GObject *object) {
+	GstObject *parent;
+
+	parent = gst_element_get_parent(object);
+
+	if (parent != NULL && GST_IS_PIPELINE(parent)) {
+		gst_bin_remove(GST_BIN(parent), GST_ELEMENT(object));
+		gst_element_set_state(GST_ELEMENT(object), GST_STATE_NULL);
+	}
+
+	G_OBJECT_CLASS(kms_media_handler_src_parent_class)->dispose(object);
+}
 
 static void
 kms_media_handler_src_class_init(KmsMediaHandlerSrcClass *klass) {
+	GObjectClass *gobject_class = G_OBJECT_CLASS(klass);
+
 	g_type_class_add_private(klass, sizeof(KmsMediaHandlerSrcPriv));
+
+	gobject_class->constructed = constructed;
+	gobject_class->finalize = finalize;
+	gobject_class->dispose = dispose;
 }
 
 static void
 kms_media_handler_src_init(KmsMediaHandlerSrc *self) {
 	self->priv = KMS_MEDIA_HANDLER_SRC_GET_PRIVATE(self);
+
 }
--- src/rtp/kms-rtp-receiver.c
+++ src/rtp/kms-rtp-receiver.c
@@ -6,13 +6,9 @@
 #define LOCK(obj) (g_static_mutex_lock(&(KMS_RTP_RECEIVER(obj)->priv->mutex)))
 #define UNLOCK(obj) (g_static_mutex_unlock(&(KMS_RTP_RECEIVER(obj)->priv->mutex)))
 
-G_LOCK_DEFINE_STATIC(class_lock);
-
 struct _KmsRtpReceiverPriv {
 	GStaticMutex mutex;
 	KmsSdpMedia *local_spec;
-	GstElement *pipe;
-	GstElement *bin;
 };
 
 enum {
@@ -28,24 +24,6 @@ dispose_local_spec(KmsRtpReceiver *self) {
 	if (self->priv->local_spec != NULL) {
 		g_object_unref(self->priv->local_spec);
 		self->priv->local_spec = NULL;
-	}
-}
-
-static void
-dispose_pipeline(KmsRtpReceiver *self) {
-	if (self->priv->pipe != NULL) {
-		g_object_unref(self->priv->pipe);
-		self->priv->pipe = NULL;
-	}
-}
-
-static void
-dispose_bin(KmsRtpReceiver *self) {
-	if (self->priv->bin != NULL) {
-		gst_bin_remove(GST_BIN(self->priv->pipe), self->priv->bin);
-		gst_element_set_state(self->priv->bin, GST_STATE_NULL);
-		g_object_unref(self->priv->bin);
-		self->priv->bin = NULL;
 	}
 }
 
@@ -86,37 +64,9 @@ get_property(GObject *object, guint property_id, GValue *value,
 	}
 }
 
-static gchar*
-get_name() {
-	static glong count = 0;
-	gchar *name;
-
-	G_LOCK(class_lock);
-	name = g_strdup_printf("%s-%ld", g_type_name(KMS_TYPE_RTP_RECEIVER),
-								count++);
-	G_UNLOCK(class_lock);
-
-	return name;
-}
-
 static void
 constructed(GObject *object) {
-	KmsRtpReceiver *self = KMS_RTP_RECEIVER(object);
-	GstElement *pipe, *bin;
-	gchar *name;
-
-	name = get_name();
-	bin = gst_bin_new(name);
-	self->priv->bin = g_object_ref(bin);
-	g_free(name);
-
-	g_object_set(bin, "async-handling", TRUE, NULL);
-	GST_OBJECT_FLAG_SET(bin, GST_ELEMENT_LOCKED_STATE);
-	gst_element_set_state(bin, GST_STATE_PLAYING);
-
-	pipe = kms_get_pipeline();
-	gst_bin_add(GST_BIN(pipe), bin);
-	self->priv->pipe = g_object_ref(pipe);
+	G_OBJECT_CLASS(kms_rtp_receiver_parent_class)->constructed(object);
 }
 
 static void
@@ -125,8 +75,6 @@ dispose(GObject *object) {
 
 	LOCK(self);
 	dispose_local_spec(self);
-	dispose_bin(self);
-	dispose_pipeline(self);
 	UNLOCK(self);
 
 	/* Chain up to the parent class */
@@ -171,6 +119,4 @@ kms_rtp_receiver_init(KmsRtpReceiver *self) {
 
 	g_static_mutex_init(&(self->priv->mutex));
 	self->priv->local_spec = NULL;
-	self->priv->pipe = NULL;
-	self->priv->bin = NULL;
 }
