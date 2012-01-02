d5c9bd910eb76966bf08ac6aa15ec3c6bd678b8f
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-01-02 09:05:15 +0100

    Use GMutex instead of GStaticMutex couse threads are initialized

--- src/rtp/kms-rtp-receiver.c
+++ src/rtp/kms-rtp-receiver.c
@@ -3,11 +3,11 @@
 
 #define KMS_RTP_RECEIVER_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), KMS_TYPE_RTP_RECEIVER, KmsRtpReceiverPriv))
 
-#define LOCK(obj) (g_static_mutex_lock(&(KMS_RTP_RECEIVER(obj)->priv->mutex)))
-#define UNLOCK(obj) (g_static_mutex_unlock(&(KMS_RTP_RECEIVER(obj)->priv->mutex)))
+#define LOCK(obj) (g_mutex_lock(KMS_RTP_RECEIVER(obj)->priv->mutex))
+#define UNLOCK(obj) (g_mutex_unlock(KMS_RTP_RECEIVER(obj)->priv->mutex))
 
 struct _KmsRtpReceiverPriv {
-	GStaticMutex mutex;
+	GMutex *mutex;
 	KmsSdpMedia *local_spec;
 };
 
@@ -85,7 +85,7 @@ static void
 finalize(GObject *object) {
 	KmsRtpReceiver *self = KMS_RTP_RECEIVER(object);
 
-	g_static_mutex_free(&(self->priv->mutex));
+	g_mutex_free(self->priv->mutex);
 
 	/* Chain up to the parent class */
 	G_OBJECT_CLASS(kms_rtp_receiver_parent_class)->finalize(object);
@@ -117,6 +117,6 @@ static void
 kms_rtp_receiver_init(KmsRtpReceiver *self) {
 	self->priv = KMS_RTP_RECEIVER_GET_PRIVATE(self);
 
-	g_static_mutex_init(&(self->priv->mutex));
+	self->priv->mutex = g_mutex_new();
 	self->priv->local_spec = NULL;
 }
