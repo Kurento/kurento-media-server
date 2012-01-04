6d01ae0f3bfeb0e7b0e3aadb73e10af59115c7cc
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-01-04 17:04:04 +0100

    Prepare test for media reception

--- test/test_rtp_transmission.c
+++ test/test_rtp_transmission.c
@@ -7,7 +7,7 @@
 
 #define CONNECTIONS 10
 
-#define TESTS 6000
+#define TESTS 60
 
 static void
 get_ports(KmsSdpSession *session, gint *a_port, gint *v_port) {
@@ -43,14 +43,24 @@ get_ports(KmsSdpSession *session, gint *a_port, gint *v_port) {
 	g_value_array_free(medias);
 }
 
+static GstElement*
+send_media(gint aport, gint vport) {
+	GstElement *pipe;
+
+	pipe = gst_pipeline_new(NULL);
+
+	return pipe;
+}
+
 static void
 test_connection() {
 	KmsEndpoint *ep;
 	KmsConnection *conn;
-	KmsSdpSession *session;
+	KmsSdpSession *session, *session2;
 	GError *err = NULL;
 	gboolean ret;
 	gint audio_port, video_port;
+	GstElement *pipe;
 	gchar *session_str;
 
 	ep = create_endpoint();
@@ -66,15 +76,22 @@ test_connection() {
 
 	g_assert(conn != NULL);
 
+	session2 = create_second_session();
+	ret = kms_connection_connect_to_remote(conn, session2, &err);
+	if (ret && err != NULL) {
+		g_printerr("%s:%d: %s\n", __FILE__, __LINE__, err->message);
+		g_error_free(err);
+	}
+	g_object_unref(session2);
+	g_assert(ret);
+
 	if (g_object_class_find_property(G_OBJECT_GET_CLASS(conn),
 		"descriptor") == NULL) {
 		g_assert_not_reached();
-		}
+	}
 
 		g_object_get(conn, "descriptor", &session, NULL);
-
 	g_assert(session != NULL);
-
 	get_ports(session, &audio_port, &video_port);
 
 	session_str = kms_sdp_session_to_string(session);
@@ -83,9 +100,11 @@ test_connection() {
 
 	g_object_unref(session);
 
-	g_print("Audio port: %d\nVideo port:%d\n", audio_port, video_port);
+	g_print("Audio port: %d\nVideo port: %d\n", audio_port, video_port);
 
-	/* TODO: Send media to the port from a different pipeline */
+	pipe = send_media(audio_port, video_port);
+
+	sleep(1);
 
 	ret = kms_endpoint_delete_connection(ep, conn, &err);
 	if (!ret && err != NULL) {
@@ -95,15 +114,31 @@ test_connection() {
 	g_assert(ret);
 	g_object_unref(conn);
 
+	gst_element_set_state(pipe, GST_STATE_NULL);
+	g_object_unref(pipe);
+
 	check_endpoint(ep);
 	g_object_unref(ep);
 }
 
 gint
 main(gint argc, gchar **argv) {
+	gint i;
+	gint new_mem, mem = 0;
+
 	kms_init(&argc, &argv);
 
-	test_connection();
+	g_print("Initial memory: %d\n", get_data_memory());
+	for (i = 0; i < TESTS; i++) {
+		test_connection();
+		sleep(1);
+		new_mem = get_data_memory();
+		g_print("%d\n", new_mem);
+
+		if (i > 1)
+			g_assert(mem == new_mem);
+		mem = new_mem;
+	}
 
 	return 0;
 }
