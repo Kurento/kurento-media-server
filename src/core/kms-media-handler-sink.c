b850f53348fcbaaa28c8d72488028471eca69d10
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-01-09 09:59:41 +0100

    Correctly initialize mutex for media-handler-sink

--- src/core/kms-media-handler-sink.c
+++ src/core/kms-media-handler-sink.c
@@ -3,11 +3,11 @@
 
 #define KMS_MEDIA_HANDLER_SINK_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), KMS_TYPE_MEDIA_HANDLER_SINK, KmsMediaHandlerSinkPriv))
 
-#define LOCK(obj) (g_static_mutex_lock(&(KMS_MEDIA_HANDLER_SINK(obj)->priv->mutex)))
-#define UNLOCK(obj) (g_static_mutex_unlock(&(KMS_MEDIA_HANDLER_SINK(obj)->priv->mutex)))
+#define LOCK(obj) (g_mutex_lock(&(KMS_MEDIA_HANDLER_SINK(obj)->priv->mutex)))
+#define UNLOCK(obj) (g_mutex_unlock(&(KMS_MEDIA_HANDLER_SINK(obj)->priv->mutex)))
 
 struct _KmsMediaHandlerSinkPriv {
-	GStaticMutex mutex;
+	GMutex *mutex;
 };
 
 enum {
@@ -33,7 +33,9 @@ constructed(GObject *object) {
 
 static void
 finalize(GObject *object) {
-	/**/
+	KmsMediaHandlerSink *self = KMS_MEDIA_HANDLER_SINK(object);
+
+	g_mutex_free(self->priv->mutex);
 
 	G_OBJECT_CLASS(kms_media_handler_sink_parent_class)->finalize(object);
 }
@@ -73,4 +75,6 @@ kms_media_handler_sink_class_init(KmsMediaHandlerSinkClass *klass) {
 static void
 kms_media_handler_sink_init(KmsMediaHandlerSink *self) {
 	self->priv = KMS_MEDIA_HANDLER_SINK_GET_PRIVATE(self);
+
+	self->priv->mutex = g_mutex_new();
 }
