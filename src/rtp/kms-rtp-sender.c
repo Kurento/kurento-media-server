61a04242856140123af98948b6b53862c994cc28
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-01-09 10:40:45 +0100

    Create and dispose rtp-sender mutex correctly

--- src/rtp/kms-rtp-sender.c
+++ src/rtp/kms-rtp-sender.c
@@ -3,11 +3,11 @@
 
 #define KMS_RTP_SENDER_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), KMS_TYPE_RTP_SENDER, KmsRtpSenderPriv))
 
-#define LOCK(obj) (g_static_mutex_lock(&(KMS_RTP_SENDER(obj)->priv->mutex)))
-#define UNLOCK(obj) (g_static_mutex_unlock(&(KMS_RTP_SENDER(obj)->priv->mutex)))
+#define LOCK(obj) (g_mutex_lock(KMS_RTP_SENDER(obj)->priv->mutex))
+#define UNLOCK(obj) (g_mutex_unlock(KMS_RTP_SENDER(obj)->priv->mutex))
 
 struct _KmsRtpSenderPriv {
-	GStaticMutex mutex;
+	GMutex *mutex;
 };
 
 enum {
@@ -23,11 +23,33 @@ kms_rtp_sender_terminate(KmsRtpSender *self) {
 }
 
 static void
+static void
+dispose(GObject *object) {
+	G_OBJECT_CLASS(kms_rtp_sender_parent_class)->dispose(object);
+}
+
+static void
+finalize(GObject *object) {
+	KmsRtpSender *self = KMS_RTP_SENDER(object);
+
+	g_mutex_free(self->priv->mutex);
+
+	G_OBJECT_CLASS(kms_rtp_sender_parent_class)->finalize(object);
+}
+
+static void
 kms_rtp_sender_class_init(KmsRtpSenderClass *klass) {
+	GObjectClass *object_class = G_OBJECT_CLASS(klass);
+
 	g_type_class_add_private(klass, sizeof(KmsRtpSenderPriv));
+
+	object_class->finalize = finalize;
+	object_class->dispose = dispose;
 }
 
 static void
 kms_rtp_sender_init(KmsRtpSender *self) {
 	self->priv = KMS_RTP_SENDER_GET_PRIVATE(self);
+
+	self->priv->mutex = g_mutex_new();
 }
