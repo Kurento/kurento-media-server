8f2cdce5a4671f4add7eb0fb339be6982d35e68f
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-04-03 13:42:35 +0200

    Add basic log on MediaService

--- server/handlers/MediaServiceHandler.cpp
+++ server/handlers/MediaServiceHandler.cpp
@@ -1,6 +1,13 @@
 #include "MediaServiceHandler.h"
+#include "log.h"
 
 using namespace ::com::kurento::kms::api;
+
+using ::com::kurento::log::Log;
+
+static Log l("MediaServiceHandler");
+#define d(...) aux_debug(l, __VA_ARGS__);
+#define i(...) aux_info(l, __VA_ARGS__);
 
 namespace com { namespace kurento { namespace kms {
 
@@ -16,10 +23,12 @@ MediaServerServiceHandler::~MediaServerServiceHandler() {
 
 
 void MediaServerServiceHandler::createMediaSession(MediaSession& _return) {
+	i("createMediaSession");
 	_return = manager->createMediaSession();
 }
 
 void MediaServerServiceHandler::deleteMediaSession(const MediaSession& session) {
+	i("deleteMediaSession");
 	manager->deleteMediaSession(session);
 }
 
