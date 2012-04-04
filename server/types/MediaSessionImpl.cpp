a2b5e7960567209321455a1bfb581830e478c65b
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-04-04 13:30:43 +0200

    Make MediaSession implement its service methods

--- server/types/MediaSessionImpl.cpp
+++ server/types/MediaSessionImpl.cpp
@@ -3,8 +3,46 @@
 #include <glibmm.h>
 
 using ::com::kurento::kms::MediaSessionImpl;
+using namespace ::com::kurento::kms::api;
 
 MediaSessionImpl::MediaSessionImpl() : MediaSession() {
 	MediaObjectImpl parent;
 	__set_parent(parent);
+}
+
+void
+MediaSessionImpl::createNetworkConnection(NetworkConnection& _return,
+		const MediaSession& mediaSession,
+		const std::vector<NetworkConnectionConfig::type>& config) {
+
+}
+
+void
+MediaSessionImpl::deleteNetworkConnection(const MediaSession& mediaSession,
+				const NetworkConnection& networConnection) {
+}
+
+void
+MediaSessionImpl::getNetworkConnections(
+				std::vector<NetworkConnection>& _return,
+				const MediaSession& mediaSession) {
+
+}
+
+void
+MediaSessionImpl::createMixer(Mixer& _return, const MediaSession& mediaSession,
+			const std::vector<MixerConfig::type>& config) {
+
+}
+
+void
+MediaSessionImpl::deleteMixer(const MediaSession& mediaSession,
+							const Mixer& mixer) {
+
+}
+
+void
+MediaSessionImpl::getMixers(std::vector<Mixer>& _return,
+					const MediaSession& mediaSession) {
+
 }
--- server/types/MediaSessionImpl.h
+++ server/types/MediaSessionImpl.h
@@ -2,16 +2,27 @@
 #define MEDIA_SESSION_IMPL
 
 #include "mediaSession_types.h"
+#include "MediaSessionService.h"
 #include "types/MediaObjectImpl.h"
 
-using ::com::kurento::kms::api::MediaSession;
+using namespace ::com::kurento::kms::api;
 
 namespace com { namespace kurento { namespace kms {
 
-class MediaSessionImpl : public MediaSession {
+class MediaSessionImpl : public MediaSession,
+			public virtual MediaObjectImpl,
+			public virtual MediaSessionServiceIf {
 public:
 	MediaSessionImpl();
 	~MediaSessionImpl() throw() {};
+
+	void createNetworkConnection(NetworkConnection& _return, const MediaSession& mediaSession, const std::vector<NetworkConnectionConfig::type> & config);
+	void deleteNetworkConnection(const MediaSession& mediaSession, const NetworkConnection& networConnection);
+	void getNetworkConnections(std::vector<NetworkConnection> & _return, const MediaSession& mediaSession);
+	void createMixer(Mixer& _return, const MediaSession& mediaSession, const std::vector<MixerConfig::type> & config);
+	void deleteMixer(const MediaSession& mediaSession, const Mixer& mixer);
+	void getMixers(std::vector<Mixer> & _return, const MediaSession& mediaSession);
+
 };
 
 }}} // com::kurento::kms
