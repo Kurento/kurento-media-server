65c04ac3150ed481ef6707ba8f30d4e02ffcdec4
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-04-04 13:02:01 +0200

    Log when a method of session service is called

--- server/handlers/MediaSessionServiceHandler.cpp
+++ server/handlers/MediaSessionServiceHandler.cpp
@@ -1,6 +1,12 @@
 #include "MediaSessionServiceHandler.h"
+#include "log.h"
 
 using namespace ::com::kurento::kms::api;
+
+using ::com::kurento::log::Log;
+
+static Log l("MediaSessionHandler");
+#define i(...) aux_info(l, __VA_ARGS__);
 
 namespace com { namespace kurento { namespace kms {
 
@@ -14,43 +20,51 @@ void
 MediaSessionServiceHandler::createNetworkConnection(NetworkConnection& _return,
 					const MediaSession& mediaSessionId,
 					const std::vector<NetworkConnectionConfig::type>& config) {
+	i("CreateNetworkConnection");
 }
 
 void
 MediaSessionServiceHandler::deleteNetworkConnection(
 				const MediaSession& mediaSessionId,
 				const NetworkConnection& networConnection) {
+	i("deleteNetworkConnection");
 }
 
 void
 MediaSessionServiceHandler::getNetworkConnections(
 					std::vector<NetworkConnection>& _return,
 					const MediaSession& mediaSessionId) {
+	i("getNetworkConnections");
 }
 
 void
 MediaSessionServiceHandler::createMixer(Mixer& _return,
 				const MediaSession& mediaSessionId,
 				const std::vector<MixerConfig::type>& config) {
+	i("createMixer");
 }
 
 void
 MediaSessionServiceHandler::deleteMixer(const MediaSession& mediaSessionId,
 							const  Mixer& mixer) {
+	i("deleteMixer");
 }
 
 void
 MediaSessionServiceHandler::getMixers(std::vector<Mixer>& _return,
 					const MediaSession& mediaSessionId) {
+	i("getMixers");
 }
 
 void
 MediaSessionServiceHandler::ping(const MediaObject& mediaObject,
 							const int32_t timeout) {
+	i("ping");
 }
 
 void
 MediaSessionServiceHandler::release(const MediaObject& mediaObject) {
+	i("release");
 }
 
 }}} // com::kurento::kms
