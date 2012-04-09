ff603e2fa35e8e1ee2744e5ed5817845fdcca666
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-04-09 09:46:08 +0200

    Add methods to joinable to handle streams

--- server/types/JoinableImpl.h
+++ server/types/JoinableImpl.h
@@ -6,6 +6,7 @@
 
 using ::com::kurento::kms::api::Joinable;
 using ::com::kurento::kms::api::Direction;
+using ::com::kurento::kms::api::StreamType;
 
 namespace com { namespace kurento { namespace kms {
 
@@ -14,10 +15,19 @@ public:
 	JoinableImpl();
 	~JoinableImpl() throw() {};
 
+	std::vector<StreamType::type> getStreams(const Joinable& joinable);
+
 	void join(const JoinableImpl& to, const Direction::type direction);
 	void unjoin(const JoinableImpl& to);
+
+	void join(const JoinableImpl& to, const StreamType::type stream, const Direction::type direction);
+	void unjoin(const JoinableImpl& to, const StreamType::type stream);
+
 	std::vector<Joinable> &getJoinees();
 	std::vector<Joinable> &getDirectionJoiness(const Direction::type direction);
+
+	std::vector<Joinable> &getJoinees(const StreamType::type stream);
+	std::vector<Joinable> &getDirectionJoiness(const StreamType::type stream, const Direction::type direction);
 };
 
 }}} // com::kurento::kms
