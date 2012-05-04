61f305b9f3f80cae509447cb904e625479789449
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-05-04 11:23:57 +0200

    Add license to source code

--- src/core/kms-connection.c
+++ src/core/kms-connection.c
@@ -1,3 +1,20 @@
+/*
+kmsc - Kurento Media Server C/C++ implementation
+Copyright (C) 2012 Tikal Technologies
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3
+as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <kms-core.h>
 
 #define KMS_CONNECTION_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), KMS_TYPE_CONNECTION, KmsConnectionPriv))
--- src/core/kms-media-handler-src.c
+++ src/core/kms-media-handler-src.c
@@ -1,3 +1,20 @@
+/*
+kmsc - Kurento Media Server C/C++ implementation
+Copyright (C) 2012 Tikal Technologies
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3
+as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <kms-core.h>
 #include "internal/kms-utils.h"
 
