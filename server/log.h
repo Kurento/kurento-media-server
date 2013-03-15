bef371615b869d40b65a8893461e0982673b280d
Miguel París Díaz <mparisdiaz@gmail.com>
2013-03-15 11:06:34 +0100

    Add log.h.

---
+++ server/log.h
@@ -0,0 +1,26 @@
+/*
+kmsc - Kurento Media Server C/C++ implementation
+Copyright (C) 2012 Tikal Technologies
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3
+as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __KURENTO_LOG__
+#define __KURENTO_LOG__
+
+#include <gst/gst.h>
+
+#define GST_CAT_DEFAULT media_server
+GST_DEBUG_CATEGORY_EXTERN (GST_CAT_DEFAULT);
+
+#endif /* __KURENTO_LOG__ */
