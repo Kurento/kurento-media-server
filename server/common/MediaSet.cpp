9c3956b725abd7528d66cce7fbcfb923fa47a373
Miguel París Díaz <mparisdiaz@gmail.com>
2013-03-22 11:14:32 +0100

    Add ConcurrentMap and MediaSet classes.
    These classes are useful to manage internal objects.
    
    Issue: #128
    Change-Id: I7136f0e992e7912200d07f78301ea5ece9281b95

---
+++ server/common/MediaSet.cpp
@@ -0,0 +1,69 @@
+/*
+ * MediaSet.cpp - Kurento Media Server
+ *
+ * Copyright (C) 2013 Kurento
+ * Contact: Miguel París Díaz <mparisdiaz@gmail.com>
+ * Contact: José Antonio Santos Cadenas <santoscadenas@kurento.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "MediaSet.hpp"
+
+namespace kurento
+{
+
+void
+MediaSet::put (MediaObject *mediaObject)
+{
+  mediaObjectMap.put (mediaObject->id, mediaObject);
+}
+
+int
+MediaSet::size ()
+{
+  return mediaObjectMap.size();
+}
+
+MediaFactory *
+MediaSet::getMediaFactory (const MediaObject &mediaObject)
+{
+  MediaObject *mo;
+  MediaFactory *mf;
+
+  mo = mediaObjectMap.getValue (mediaObject.id);
+  mf = dynamic_cast<MediaFactory *> (mo);
+
+  if (mf == NULL )
+    throw MediaObjectNotFoundException();
+
+  return mf;
+}
+
+MediaPlayer *
+MediaSet::getMediaPlayer (const MediaObject &mediaObject)
+{
+  MediaObject *mo;
+  MediaPlayer *mp;
+
+  mo = mediaObjectMap.getValue (mediaObject.id);
+  mp = dynamic_cast<MediaPlayer *> (mo);
+
+  if (mp == NULL)
+    throw MediaObjectNotFoundException();
+
+  return mp;
+}
+
+} // kurento
+
---
+++ server/common/MediaSet.hpp
@@ -0,0 +1,47 @@
+/*
+ * MediaSet.hpp - Kurento Media Server
+ *
+ * Copyright (C) 2013 Kurento
+ * Contact: Miguel París Díaz <mparisdiaz@gmail.com>
+ * Contact: José Antonio Santos Cadenas <santoscadenas@kurento.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __MEDIA_SET_H__
+#define __MEDIA_SET_H__
+
+#include "ConcurrentMap.hpp"
+#include "types/MediaFactory.hpp"
+#include "types/MediaPlayer.hpp"
+
+namespace kurento
+{
+
+class MediaSet
+{
+public:
+  MediaSet () {};
+
+  void put (MediaObject *mediaObject);
+  int size();
+  MediaFactory *getMediaFactory (const MediaObject &mediaObject);
+  MediaPlayer *getMediaPlayer (const MediaObject &mediaObject);
+
+private:
+  ConcurrentMap<ObjectId, MediaObject *> mediaObjectMap;
+};
+
+} // kurento
+
+#endif /* __MEDIA_SET_H__ */
