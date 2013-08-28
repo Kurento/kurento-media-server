4eb14382f61f789b4dc899233f14fa805c29c683
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2013-08-28 11:10:58 +0200

    Add an intermediate element to allow loopback connections
    Change-Id: I217c574cb1f536f7a448444072cabbed231639a9

--- server/types/MediaSink.cpp
+++ server/types/MediaSink.cpp
@@ -56,12 +56,65 @@ MediaSink::getPadName ()
     return "video_sink";
 }
 
+static void
+remove_from_parent (GstElement *element)
+{
+  GstBin *parent = GST_BIN (GST_OBJECT_PARENT (element) );
+
+  if (parent == NULL)
+    return;
+
+  gst_object_ref (element);
+  gst_bin_remove (parent, element);
+  gst_element_set_state (element, GST_STATE_NULL);
+
+  gst_object_unref (element);
+}
+
+static void
+sink_unlinked (GstPad *pad, GstPad *peer, GstElement *filter)
+{
+  GstPad *src;
+  GstPad *src_peer;
+
+  src = gst_element_get_static_pad (filter, "src");
+  src_peer = gst_pad_get_peer (src);
+
+  if (src_peer != NULL) {
+    gst_pad_unlink (src, src_peer);
+    gst_object_unref (src_peer);
+  } else {
+    remove_from_parent (filter);
+  }
+
+  gst_object_unref (src);
+}
+
+static void
+src_unlinked (GstPad *pad, GstPad *peer, GstElement *filter)
+{
+  GstPad *sink;
+  GstPad *sink_peer;
+
+  sink = gst_element_get_static_pad (filter, "sink");
+  sink_peer = gst_pad_get_peer (sink);
+
+  if (sink_peer != NULL) {
+    gst_pad_unlink (sink_peer, sink);
+    gst_object_unref (sink_peer);
+  } else {
+    remove_from_parent (filter);
+  }
+
+  gst_object_unref (sink);
+}
+
 bool
 MediaSink::linkPad (std::shared_ptr<MediaSrc> mediaSrc, GstPad *src)
 {
   std::shared_ptr<MediaSrc> connectedSrcLocked;
   GstPad *sink;
-  bool ret;
+  bool ret = false;
 
   mutex.lock();
 
@@ -77,13 +130,58 @@ MediaSink::linkPad (std::shared_ptr<MediaSrc> mediaSrc, GstPad *src)
     unlink (connectedSrcLocked, sink);
   }
 
-  if (gst_pad_link (src, sink) == GST_PAD_LINK_OK) {
-    ret = true;
+  if (mediaSrc->parent == parent) {
+    GstBin *container;
+    GstElement *filter, *parent;
+    GstPad *aux_sink, *aux_src;
+
+    GST_DEBUG ("Connecting loopback, adding a capsfilter to allow connection");
+    parent = GST_ELEMENT (GST_OBJECT_PARENT (sink) );
+
+    if (parent == NULL)
+      goto end;
+
+    container = GST_BIN (GST_OBJECT_PARENT (parent) );
+
+    if (container == NULL)
+      goto end;
+
+    filter = gst_element_factory_make ("capsfilter", NULL);
+
+    aux_sink = gst_element_get_static_pad (filter, "sink");
+    aux_src = gst_element_get_static_pad (filter, "src");
+
+    g_signal_connect (G_OBJECT (aux_sink), "unlinked", G_CALLBACK (sink_unlinked), filter );
+    g_signal_connect (G_OBJECT (aux_src), "unlinked", G_CALLBACK (src_unlinked), filter );
+
+    gst_bin_add (container, filter);
+    gst_element_sync_state_with_parent (filter);
+
+    if (gst_pad_link (aux_src, sink) == GST_PAD_LINK_OK) {
+      if (gst_pad_link (src, aux_sink) == GST_PAD_LINK_OK)
+        ret = true;
+      else
+        gst_pad_unlink (aux_src, sink);
+
+    }
+
+    g_object_unref (aux_sink);
+    g_object_unref (aux_src);
+
+    gst_debug_bin_to_dot_file_with_ts (GST_BIN (container), GST_DEBUG_GRAPH_SHOW_ALL, "loopback");
+
+  } else {
+    if (gst_pad_link (src, sink) == GST_PAD_LINK_OK)
+      ret = true;
+  }
+
+  if (ret == true) {
     connectedSrc = std::weak_ptr<MediaSrc> (mediaSrc);
   } else {
     gst_element_release_request_pad (getElement(), sink);
-    ret = false;
   }
+
+end:
 
   g_object_unref (sink);
 
