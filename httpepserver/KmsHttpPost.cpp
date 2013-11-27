492da98d429d18befbcc87184f99dea0391d4a9a
Santiago Carot-Nemesio <sancane@gmail.com>
2013-11-27 11:59:28 +0100

    KmsHttpPost: Add Message property
    Change-Id: I82142f4dc9eb9ff21bda92b316f89cfebc9ece72

--- httpepserver/KmsHttpPost.cpp
+++ httpepserver/KmsHttpPost.cpp
@@ -12,7 +12,7 @@
  * Lesser General Public License for more details.
  *
  */
-
+#include <libsoup/soup.h>
 #include "KmsHttpPost.h"
 
 #define OBJECT_NAME "HttpPost"
@@ -22,6 +22,7 @@ GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
 
 #define KMS_HTTP_POST_GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), KMS_TYPE_HTTP_POST, KmsHttpPostPrivate))
 struct _KmsHttpPostPrivate {
+  SoupMessage *msg;
   guint id;
 };
 
@@ -31,6 +32,18 @@ G_DEFINE_TYPE_WITH_CODE (KmsHttpPost, kms_http_post,
                          G_TYPE_OBJECT,
                          GST_DEBUG_CATEGORY_INIT (GST_CAT_DEFAULT, OBJECT_NAME,
                              0, "debug category for " OBJECT_NAME " element") )
+/* properties */
+enum {
+  PROP_0,
+
+  PROP_MESSAGE,
+  N_PROPERTIES
+};
+
+#define KMS_HTTP_PORT_DEFAULT_MESSAGE NULL
+
+static GParamSpec *obj_properties[N_PROPERTIES] = { NULL, };
+
 /* signals */
 enum {
   GOT_DATA,
@@ -40,8 +53,55 @@ enum {
 static guint obj_signals[LAST_SIGNAL] = { 0 };
 
 static void
+kms_http_post_set_property (GObject *obj, guint prop_id,
+                            const GValue *value, GParamSpec *pspec)
+{
+  KmsHttpPost *self = KMS_HTTP_POST (obj);
+
+  switch (prop_id) {
+  case PROP_MESSAGE:
+    if (self->priv->msg != NULL)
+      g_object_unref (self->priv->msg);
+
+    /* TODO: Connect to "got-chunk" signal */
+    self->priv->msg = SOUP_MESSAGE (g_object_ref (g_value_get_object (value) ) );
+    break;
+
+  default:
+    /* We don't have any other property... */
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, prop_id, pspec);
+    break;
+  }
+}
+
+static void
+kms_http_post_get_property (GObject *obj, guint prop_id, GValue *value,
+                            GParamSpec *pspec)
+{
+  KmsHttpPost *self = KMS_HTTP_POST (obj);
+
+  switch (prop_id) {
+  case PROP_MESSAGE:
+    g_value_set_object (value, self->priv->msg);
+    break;
+
+  default:
+    /* We don't have any other property... */
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, prop_id, pspec);
+    break;
+  }
+}
+
+static void
 kms_http_post_dispose (GObject *obj)
 {
+  KmsHttpPost *self = KMS_HTTP_POST (obj);
+
+  if (self->priv->msg != NULL) {
+    g_object_unref ( G_OBJECT (self->priv->msg) );
+    self->priv->msg = NULL;
+  }
+
   /* Chain up to the parent class */
   G_OBJECT_CLASS (kms_http_post_parent_class)->dispose (obj);
 }
@@ -58,8 +118,21 @@ kms_http_post_class_init (KmsHttpPostClass *klass)
 {
   GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
 
+  gobject_class->set_property = kms_http_post_set_property;
+  gobject_class->get_property = kms_http_post_get_property;
   gobject_class->dispose = kms_http_post_dispose;
   gobject_class->finalize = kms_http_post_finalize;
+
+  obj_properties[PROP_MESSAGE] =
+    g_param_spec_object ("soup-message",
+                         "Soup message object",
+                         "Message to get data from",
+                         SOUP_TYPE_MESSAGE,
+                         (GParamFlags) (G_PARAM_READWRITE) );
+
+  g_object_class_install_properties (gobject_class,
+                                     N_PROPERTIES,
+                                     obj_properties);
 
   obj_signals[GOT_DATA] =
     g_signal_new ("got-data",
