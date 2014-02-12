b1360cef59388e524e06c9166781cda29f76752b
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2014-02-12 22:05:35 +0100

    Make RtpEndPoint a module
    Change-Id: I31ef65d631eab18003bcf8420e5aa301de020b14

--- server/types/MediaPipeline.cpp
+++ server/types/MediaPipeline.cpp
@@ -20,8 +20,6 @@
 #include "KmsMediaDataType_constants.h"
 #include "KmsMediaErrorCodes_constants.h"
 
-#include "KmsMediaRtpEndPointType_constants.h"
-#include "RtpEndPoint.hpp"
 #include "KmsMediaHttpGetEndPointType_constants.h"
 #include "HttpGetEndPoint.hpp"
 #include "KmsMediaHttpPostEndPointType_constants.h"
@@ -134,10 +132,6 @@ throw (KmsMediaServerException)
                                            shared_from_this (),
                                            params);
     element = std::dynamic_pointer_cast<MediaElement> (obj);
-  } else if (g_KmsMediaRtpEndPointType_constants.TYPE_NAME.compare (
-               elementType) == 0) {
-    element = std::shared_ptr<RtpEndPoint> (new RtpEndPoint (getMediaSet(),
-                                            shared_from_this (), params) );
   } else if (g_KmsMediaHttpPostEndPointType_constants.TYPE_NAME.compare (
                elementType) == 0) {
     element = std::shared_ptr<HttpPostEndPoint> (new HttpPostEndPoint (
