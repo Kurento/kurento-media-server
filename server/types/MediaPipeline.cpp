0c3e02ce1ff2d510c178bab46112a5f38c00c843
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2014-02-27 12:10:34 +0100

    Revert "Make HttpGetEndPoint a module"
    This reverts commit 8cf4e82ed69855b5550e1a451c7018e387d30968.
    
    Change-Id: I6262ab46b89cbca6db53e9838acae9b679956ff6

--- server/types/MediaPipeline.cpp
+++ server/types/MediaPipeline.cpp
@@ -20,6 +20,8 @@
 #include "KmsMediaDataType_constants.h"
 #include "KmsMediaErrorCodes_constants.h"
 
+#include "KmsMediaHttpGetEndPointType_constants.h"
+#include "HttpGetEndPoint.hpp"
 #include "KmsMediaHttpPostEndPointType_constants.h"
 #include "HttpPostEndPoint.hpp"
 #include "KmsMediaErrorCodes_constants.h"
@@ -121,6 +123,10 @@ throw (KmsMediaServerException)
     element = std::shared_ptr<HttpPostEndPoint> (new HttpPostEndPoint (
                 getMediaSet(),
                 shared_from_this (), params) );
+  } else if (g_KmsMediaHttpGetEndPointType_constants.TYPE_NAME.compare (
+               elementType) == 0) {
+    element = std::shared_ptr<HttpGetEndPoint> (new HttpGetEndPoint (getMediaSet(),
+              shared_from_this (), params) );
   } else {
     KmsMediaServerException except;
 
