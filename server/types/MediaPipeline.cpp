558ca08a4f850012d3c7694bae2436788b7aa9b4
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2014-02-27 12:10:45 +0100

    Revert "Make ZBarFilter a module"
    This reverts commit bcb6bf1d47a17efb9e9ff09398f0002409e8ab15.
    
    Change-Id: I8a2cedd7d9e7464f6dd2530a420cf82276b1af97

--- server/types/MediaPipeline.cpp
+++ server/types/MediaPipeline.cpp
@@ -24,6 +24,8 @@
 #include "HttpGetEndPoint.hpp"
 #include "KmsMediaHttpPostEndPointType_constants.h"
 #include "HttpPostEndPoint.hpp"
+#include "KmsMediaZBarFilterType_constants.h"
+#include "ZBarFilter.hpp"
 #include "KmsMediaJackVaderFilterType_constants.h"
 #include "JackVaderFilter.hpp"
 #include "KmsMediaPointerDetectorFilterType_constants.h"
@@ -139,6 +141,10 @@ throw (KmsMediaServerException)
                elementType) == 0) {
     element = std::shared_ptr<HttpGetEndPoint> (new HttpGetEndPoint (getMediaSet(),
               shared_from_this (), params) );
+  } else if (g_KmsMediaZBarFilterType_constants.TYPE_NAME.compare (
+               elementType) == 0) {
+    element = std::shared_ptr<ZBarFilter> (new ZBarFilter (getMediaSet(),
+                                           shared_from_this (), params) );
   } else if (g_KmsMediaJackVaderFilterType_constants.TYPE_NAME.compare (
                elementType) == 0) {
     element = std::shared_ptr<JackVaderFilter> (new JackVaderFilter (
