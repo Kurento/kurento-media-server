1057baf483134e1b60bcbcb49756ed29dc006ad0
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2014-04-07 09:15:11 +0200

    KmsHttpLoop: Thread should hold its own reference of context and loop
    Change-Id: Id705eb419eaee5ac6bcd7737b06f503ded4f9478

--- httpepserver/KmsHttpLoop.cpp
+++ httpepserver/KmsHttpLoop.cpp
@@ -62,7 +62,6 @@ quit_main_loop (KmsHttpLoop *self)
   GST_DEBUG ("Exiting main loop");
 
   g_main_loop_quit (self->priv->loop);
-  g_main_context_release (self->priv->context);
 
   return G_SOURCE_REMOVE;
 }
@@ -76,9 +75,9 @@ loop_thread_init (gpointer data)
 
   KMS_HTTP_LOOP_LOCK (self);
   self->priv->context = g_main_context_new ();
-  context = self->priv->context;
+  context = g_main_context_ref (self->priv->context);
   self->priv->loop = g_main_loop_new (context, FALSE);
-  loop = self->priv->loop;
+  loop = g_main_loop_ref (self->priv->loop);
   KMS_HTTP_LOOP_UNLOCK (self);
 
   /* unlock main process because context is already initialized */
@@ -94,9 +93,12 @@ loop_thread_init (gpointer data)
 
   GST_DEBUG ("Running main loop");
   g_main_loop_run (loop);
+  g_main_context_release (context);
 
 end:
   GST_DEBUG ("Thread finished");
+  g_main_loop_unref (loop);
+  g_main_context_unref (context);
 
   return NULL;
 }
