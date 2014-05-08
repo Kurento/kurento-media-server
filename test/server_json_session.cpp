eaa4b24021a523e143744bf2248bc3ca5630f366
Santiago Carot-Nemesio <sancane@gmail.com>
2014-05-08 14:22:09 +0200

    Implement test to check unrefs of elements from direfferent sessions
    Change-Id: I7d943fe86815081d4d1c713a3471fbd777bfe72a

--- test/server_json_session.cpp
+++ test/server_json_session.cpp
@@ -24,6 +24,10 @@
 GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
 #define GST_DEFAULT_NAME "server_json_session"
 
+#define VIDEO_URI "https://ci.kurento.com/video/sintel.webm"
+#define RECORDER_URI_1 "file:///tmp/recorder1.webm"
+#define RECORDER_URI_2 "file:///tmp/recorder2.webm"
+
 class ClientHandler : public F
 {
 public:
@@ -38,7 +42,280 @@ protected:
 void
 ClientHandler::check_create_pipeline_call()
 {
-  /*TODO: */
+  Json::Value request;
+  Json::Value response;
+  Json::FastWriter writer;
+  Json::Reader reader;
+  std::string req_str;
+  std::string response_str;
+
+  std::string pipeId;
+  std::string playerId;
+  std::string filterId;
+  std::string recorderId1;
+  std::string recorderId2;
+
+  Json::Value params;
+  Json::Value constructorParams;
+  Json::Value operationParams;
+
+  std::string sessionId1 = "1111";
+  std::string sessionId2 = "2222";
+
+  request["jsonrpc"] = "2.0";
+  request["id"] = 0;
+  request["method"] = "create";
+
+  /* Create a new piepeline using session 1 */
+
+  params["type"] = "MediaPipeline";
+  params["sessionId"] = sessionId1;
+
+  request["params"] = params;
+
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+
+  BOOST_CHECK (reader.parse (response_str, response) == true);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
+  BOOST_CHECK (response["result"].isObject() );
+  BOOST_CHECK (response["result"].isMember ("value") );
+  BOOST_CHECK (response["result"]["value"].isString() );
+
+  pipeId = response["result"]["value"].asString();
+  response_str.clear();
+
+  /* Create a player using session 1 */
+
+  params["type"] = "PlayerEndpoint";
+  constructorParams ["mediaPipeline"] = pipeId;
+  constructorParams ["uri"] = VIDEO_URI;
+  params["constructorParams"] = constructorParams;
+
+  request["params"] = params;
+
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+
+  BOOST_CHECK (reader.parse (response_str, response) == true);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
+  BOOST_CHECK (response["result"].isObject() );
+  BOOST_CHECK (response["result"].isMember ("value") );
+  BOOST_CHECK (response["result"]["value"].isString() );
+
+  playerId = response["result"]["value"].asString();
+  constructorParams.clear();
+  response_str.clear();
+
+  /* Create a filter using session 1 */
+
+  params["type"] = "ZBarFilter";
+  constructorParams ["mediaPipeline"] = pipeId;
+  params["constructorParams"] = constructorParams;
+
+  request["params"] = params;
+
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+
+  BOOST_CHECK (reader.parse (response_str, response) == true);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
+  BOOST_CHECK (response["result"].isObject() );
+  BOOST_CHECK (response["result"].isMember ("value") );
+  BOOST_CHECK (response["result"]["value"].isString() );
+
+  filterId = response["result"]["value"].asString();
+  constructorParams.clear();
+  response_str.clear();
+
+  /* Create a recorder using session 1 */
+
+  params["type"] = "RecorderEndpoint";
+  constructorParams ["mediaPipeline"] = pipeId;
+  constructorParams ["uri"] = RECORDER_URI_1;
+  params["constructorParams"] = constructorParams;
+
+  request["params"] = params;
+
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+
+  BOOST_CHECK (reader.parse (response_str, response) == true);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
+  BOOST_CHECK (response["result"].isObject() );
+  BOOST_CHECK (response["result"].isMember ("value") );
+  BOOST_CHECK (response["result"]["value"].isString() );
+
+  recorderId1 = response["result"]["value"].asString();
+  constructorParams.clear();
+  response_str.clear();
+
+  /* Create a recorder using session 2 */
+
+  params["type"] = "RecorderEndpoint";
+  params["sessionId"] = sessionId2;
+  constructorParams ["mediaPipeline"] = pipeId;
+  constructorParams ["uri"] = RECORDER_URI_2;
+  params["constructorParams"] = constructorParams;
+
+  request["params"] = params;
+
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+
+  BOOST_CHECK (reader.parse (response_str, response) == true);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
+  BOOST_CHECK (response["result"].isObject() );
+  BOOST_CHECK (response["result"].isMember ("value") );
+  BOOST_CHECK (response["result"]["value"].isString() );
+
+  recorderId2 = response["result"]["value"].asString();
+  constructorParams.clear();
+  response_str.clear();
+
+  GST_DEBUG ("Pipeline: %s", pipeId.c_str() );
+  GST_DEBUG ("Player: %s", playerId.c_str() );
+  GST_DEBUG ("Filter: %s", filterId.c_str() );
+  GST_DEBUG ("Recorder1: %s", recorderId1.c_str() );
+  GST_DEBUG ("Recorder2: %s", recorderId2.c_str() );
+
+  /* Link player and filter using session 1 */
+
+  request["method"] = "invoke";
+  params.clear();
+  params["sessionId"] = sessionId1;
+  params["object"] = playerId;
+  params["operation"] = "connect";
+
+  operationParams["sink"] = filterId;
+
+  params["operationParams"] = operationParams;
+  request["params"] = params;
+
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
+  BOOST_CHECK (response["result"].isObject() );
+
+  params.clear();
+  operationParams.clear();
+  response_str.clear();
+
+  /* Link filter and recorder1 using session 1 */
+
+  request["method"] = "invoke";
+  params.clear();
+  params["sessionId"] = sessionId1;
+  params["object"] = filterId;
+  params["operation"] = "connect";
+
+  operationParams["sink"] = recorderId1;
+
+  params["operationParams"] = operationParams;
+  request["params"] = params;
+
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
+  BOOST_CHECK (response["result"].isObject() );
+
+  params.clear();
+  operationParams.clear();
+  response_str.clear();
+
+  /* Link filter and recorder2 using session 2 */
+
+  request["method"] = "invoke";
+  params.clear();
+  params["sessionId"] = sessionId2;
+  params["object"] = filterId;
+  params["operation"] = "connect";
+
+  operationParams["sink"] = recorderId2;
+
+  params["operationParams"] = operationParams;
+  request["params"] = params;
+
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
+  BOOST_CHECK (response["result"].isObject() );
+
+  params.clear();
+  operationParams.clear();
+  response_str.clear();
+
+  /* Unref recorder1 using session 1 */
+
+  request["method"] = "unref";
+  params["sessionId"] = sessionId1;
+  params["object"] = recorderId1;
+
+  request["params"] = params;
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+  BOOST_CHECK (reader.parse (response_str, response) == true);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
+
+  /* Unref filter using session 1 */
+
+  request["method"] = "unref";
+  params["sessionId"] = sessionId1;
+  params["object"] = filterId;
+
+  request["params"] = params;
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+  BOOST_CHECK (reader.parse (response_str, response) == true);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
+
+  /* Unref player using session 1 */
+
+  request["method"] = "unref";
+  params["sessionId"] = sessionId1;
+  params["object"] = playerId;
+
+  request["params"] = params;
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+  BOOST_CHECK (reader.parse (response_str, response) == true);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
+
+  /* Unref recorder2 using session 2 */
+
+  request["method"] = "unref";
+  params["sessionId"] = sessionId2;
+  params["object"] = recorderId2;
+
+  request["params"] = params;
+  req_str = writer.write (request);
+  client->invokeJsonRpc (response_str, req_str);
+  BOOST_CHECK (reader.parse (response_str, response) == true);
+
+  BOOST_CHECK (!response.isMember ("error") );
+  BOOST_CHECK (response.isMember ("result") );
 }
 
 BOOST_FIXTURE_TEST_SUITE ( server_json_session_test_suite, ClientHandler)
