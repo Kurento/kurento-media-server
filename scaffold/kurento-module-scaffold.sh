6f53e347f9c7575aba8dd0af946a322c82874d16
David Fernandez <d.fernandezlop@gmail.com>
2014-09-05 12:35:57 +0200

    Add debian package generation in modules generated by scaffold
    Change-Id: Ib5683844c0da89fc951eb40cf9e1d37e0012bd94

--- scaffold/kurento-module-scaffold.sh
+++ scaffold/kurento-module-scaffold.sh
@@ -16,6 +16,7 @@ nameUndersScoreLower=`echo $camelName | sed -e 's/\([A-Z]\)/_\L\1/g' -e 's/^_//'
 nameScoreLower=`echo $camelName | sed -e 's/\([A-Z]\)/-\L\1/g' -e 's/^-//'`
 nameUnderscoreUpper=`echo $nameUndersScoreLower | sed -e 's/\([a-z]\)/\U\1/g'`
 outputDir=$2
+openCVFilter=$3
 
 #Change to output directory
 mkdir -p $outputDir
@@ -32,7 +33,7 @@ cd src
 mkdir server
 
 cat $currentDir/CMakeLists_server.txt.template >> "CMakeLists.txt"
-if [ -z "$3" ]
+if [ -z "$openCVFilter" ]
   then
     mkdir gst-plugins
     cat $currentDir/CMakeLists_server_plugin.txt.template >> "CMakeLists.txt"
@@ -56,7 +57,7 @@ cd interface
 
 cat $currentDir/filter.kmd.json.template | sed -e "s/\${name}/$name/" >> "$name.kmd.json"
 
-if [ -z "$3" ]
+if [ -z "$openCVFilter" ]
   then
     cat $currentDir/filter.Filter.kmd.json.template | sed -e "s/\${name}/$name/" -e "s/\${camelName}/$camelName/" -e "s/\${extends}/Filter/">> "$name.$camelName.kmd.json"
 else
@@ -69,12 +70,37 @@ cat $currentDir/CMakeLists_src.txt.template >> "CMakeLists.txt"
 cd ../..
 
 cat $currentDir/config.h.cmake.template | sed -e "s/\${nameUnderscoreUpper}/$nameUnderscoreUpper/" >> "config.h.cmake"
-cat $currentDir/CMakeLists_root.txt.template | sed -e "s/\${name}/$name/" >> "CMakeLists.txt"
+cat $currentDir/CMakeLists_root.txt.template | sed -e "s/\${nameScoreLower}/$nameScoreLower/" >> "CMakeLists.txt"
 
+#create debian directory
+mkdir debian
+cd debian
+echo "9" > "compat"
+touch copyright
+touch docs
+
+cat $currentDir/changelog.template | sed -e "s/\${nameScoreLower}/$nameScoreLower/" -e "s/\${date}/`date -R`/">> "changelog"
+cat $currentDir/control.template | sed -e "s/\${nameScoreLower}/$nameScoreLower/" >> "control"
+cat $currentDir/rules.template >> "rules"
+
+if [ -z "$openCVFilter" ]
+  then
+    cat $currentDir/name.install.template >> "$nameScoreLower.install"
+else
+  cat $currentDir/name.install.OpenCV.template >> "$nameScoreLower.install"
+fi
+
+cat $currentDir/name-dev.install.template >> "$nameScoreLower-dev.install"
+
+mkdir source
+cd source
+echo "3.0 (native)" >> "format"
+#create .gitignore file
+cd ../..
 echo "build/" >> ".gitignore"
+echo "debian/$nameUndersScoreLower" >> ".gitignore"
 
-git add CMakeLists.txt src .gitignore config.h.cmake
-git add CMakeLists.txt src .gitignore
+git add CMakeLists.txt src .gitignore config.h.cmake debian
 git commit -m "Initial commit"
 
 echo "Folder tree created successfully"
