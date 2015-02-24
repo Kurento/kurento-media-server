d197c4736f5d8a305389025dca4c1ac5f0698281
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2015-02-24 16:09:00 +0100

    Allow loading different config files for external modules
    Change-Id: I9881d076f5286435065fb88d781e106a7c0c035d

--- server/main.cpp
+++ server/main.cpp
@@ -15,6 +15,9 @@
 
 #include <config.h>
 
+#include <queue>
+#include <boost/foreach.hpp>
+
 #include <glibmm.h>
 #include <fstream>
 #include <iostream>
@@ -51,8 +54,159 @@ namespace logging = boost::log;
 
 Glib::RefPtr<Glib::MainLoop> loop = Glib::MainLoop::create ();
 
+static std::list<std::string>
+split (const std::string &s, char delim)
+{
+  std::list<std::string> elems;
+  std::stringstream ss (s);
+  std::string item;
+
+  while (std::getline (ss, item, delim) ) {
+    elems.push_back (item);
+  }
+
+  return elems;
+}
+
+static void
+mergePropertyTrees (boost::property_tree::ptree &ptMerged,
+                    const boost::property_tree::ptree &rptSecond )
+{
+  // Keep track of keys and values (subtrees) in second property tree
+  std::queue<std::string> qKeys;
+  std::queue<boost::property_tree::ptree> qValues;
+  qValues.push ( rptSecond );
+
+  // Iterate over second property tree
+  while ( !qValues.empty() ) {
+    // Setup keys and corresponding values
+    boost::property_tree::ptree ptree = qValues.front();
+    qValues.pop();
+    std::string keychain;
+
+    if ( !qKeys.empty() ) {
+      keychain = qKeys.front();
+      qKeys.pop();
+    }
+
+    BOOST_FOREACH ( const boost::property_tree::ptree::value_type & child, ptree ) {
+      if ( child.second.size() == 0 ) {
+        std::string s;
+
+        if ( !keychain.empty() ) {
+          s = keychain + "." + child.first.data();
+        } else {
+          s = child.first.data();
+        }
+
+        // Put into combined property tree
+        ptMerged.put ( s, child.second.data() );
+      } else {
+        // Put keys (identifiers of subtrees) and all of its parents (where present)
+        // aside for later iteration.
+        if ( !keychain.empty() ) {
+          qKeys.push ( keychain + "." + child.first.data() );
+        } else {
+          qKeys.push ( child.first.data() );
+        }
+
+        // Put values (the subtrees) aside, too
+        qValues.push ( child.second );
+      }
+    }
+  }
+}
+
+static void
+loadModulesConfigFromDir (boost::property_tree::ptree &config,
+                          const boost::filesystem::path &dir, const boost::filesystem::path &parentDir)
+{
+  GST_INFO ("Looking for config files in %s", dir.string().c_str() );
+
+  if (!boost::filesystem::is_directory (dir) ) {
+    GST_WARNING ("Unable to load config files from: %s, it is not a directory",
+                 dir.string().c_str() );
+    return;
+  }
+
+  boost::filesystem::directory_iterator end_itr;
+
+  for ( boost::filesystem::directory_iterator itr ( dir ); itr != end_itr;
+        ++itr ) {
+    if (boost::filesystem::is_regular (*itr) ) {
+      boost::filesystem::path extension = itr->path().extension();
+      boost::filesystem::path extension2 = itr->path().stem().extension();
+      std::string fileName = itr->path().filename().string();
+
+      if (extension.string() == ".json" && extension2.string() == ".conf") {
+        boost::property_tree::ptree moduleConfig;
+
+        boost::property_tree::read_json (itr->path().string(), moduleConfig);
+
+        moduleConfig.add ("configPath", itr->path().parent_path().string() );
+
+        {
+          boost::filesystem::path diffpath;
+
+          boost::filesystem::path tmppath = itr->path().parent_path();
+
+          while (tmppath != parentDir) {
+            diffpath = tmppath.stem() / diffpath;
+            tmppath = tmppath.parent_path();
+          }
+
+          tmppath = diffpath;
+
+          boost::property_tree::ptree loadedConfig;
+          std::string key = "modules";
+
+          for (auto it = tmppath.begin(); it != tmppath.end(); it ++) {
+            key += "." + it->string();
+          }
+
+          fileName = fileName.substr (0, fileName.size() - extension.string().size() );
+          fileName = fileName.substr (0, fileName.size() - extension2.string().size() );
+
+          key += "." + fileName;
+
+          loadedConfig.put_child (key, moduleConfig);
+
+          mergePropertyTrees (config, loadedConfig);
+        }
+
+        GST_INFO ("Loaded module config from: %s", itr->path().string().c_str() );
+      }
+    } else if (boost::filesystem::is_directory (*itr) ) {
+      loadModulesConfigFromDir (config, itr->path(), parentDir);
+    }
+  }
+}
+
+static void
+loadModulesConfig (boost::property_tree::ptree &config,
+                   const boost::filesystem::path &configFilePath, std::string modulesConfigPath)
+{
+  std::list <std::string> locations;
+
+  if (modulesConfigPath.empty() ) {
+    boost::filesystem::path modulesConfigDir = configFilePath.parent_path() /
+        "modules";
+
+    modulesConfigPath = modulesConfigDir.string();
+  }
+
+  locations = split (modulesConfigPath, ':');
+
+  for (std::string location : locations) {
+    boost::filesystem::path dir (location);
+
+    loadModulesConfigFromDir (config, dir, dir);
+  }
+}
+
 static std::shared_ptr<Transport>
-load_config (boost::property_tree::ptree &config, const std::string &file_name)
+load_config (boost::property_tree::ptree &config, const std::string &file_name,
+             const std::string &modulesConfigPath)
 {
   std::shared_ptr<Transport> transport;
   boost::filesystem::path configFilePath (file_name);
@@ -66,9 +220,17 @@ load_config (boost::property_tree::ptree &config, const std::string &file_name)
     exit (1);
   }
 
+  loadModulesConfig (config, configFilePath, modulesConfigPath);
+
   config.add ("configPath", configFilePath.parent_path().string() );
 
   GST_INFO ("Configuration loaded successfully");
+
+  std::ostringstream oss;
+  boost::property_tree::write_json (oss, config, true);
+  std::string jsonConfig = oss.str();
+
+  GST_DEBUG ("Effective loaded config:\n%s", jsonConfig.c_str() );
 
   std::shared_ptr<ServerMethods> serverMethods (new ServerMethods (config) );
 
@@ -134,7 +296,7 @@ main (int argc, char **argv)
   std::shared_ptr<Transport> transport;
   boost::property_tree::ptree config;
   std::string confFile;
-  std::string path, logs_path;
+  std::string path, logs_path, modulesConfigPath;
 
   kms_init_dependencies (argc, argv);
 
@@ -151,7 +313,10 @@ main (int argc, char **argv)
      (&confFile)->default_value (DEFAULT_CONFIG_FILE),
      "Configuration file location")
     ("logs-path,d", boost::program_options::value <std::string> (&logs_path),
-     "Path where debug files will be stored");
+     "Path where debug files will be stored")
+    ("modules-config-path,c",
+     boost::program_options::value <std::string> (&modulesConfigPath),
+     "Path where modules config files can be found");
 
     boost::program_options::command_line_parser clp (argc, argv);
     clp.options (desc).allow_unregistered();
@@ -233,7 +398,7 @@ main (int argc, char **argv)
 
   GST_INFO ("Kmsc version: %s", get_version () );
 
-  transport = load_config (config, confFile);
+  transport = load_config (config, confFile, modulesConfigPath);
 
   /* Start transport */
   transport->start ();
