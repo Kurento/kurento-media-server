4e80d8e015ebd7593a966cde8baaa50a55e02e4c
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2016-01-14 09:12:11 +0100

    death_handler: Fix compilation error when -O2 is enabled
    Change-Id: Ifad7effb7feda3b8d11f03af769bc64a03e87a4d

--- server/death_handler.cpp
+++ server/death_handler.cpp
@@ -772,7 +772,7 @@ void DeathHandler::SignalHandler (int sig, void * /* info */, void *secret)
 
   // Write '\0' to indicate the end of the output
   char end = '\0';
-  write (STDERR_FILENO, &end, 1);
+  checked (write (STDERR_FILENO, &end, 1) );
 
   if (thread_safe_) {
     // Resume the parent process
